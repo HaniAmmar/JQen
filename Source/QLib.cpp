5c73da6fed41b8b23f40817d5cd5338c44b11f8d
Hani Ammar <hani.code@outlook.com>
2020-09-30 18:54:02 +0300

    Update Qlib

--- Source/QLib.cpp
+++ Source/QLib.cpp
@@ -10,7 +10,7 @@ std::string JQen_Render(const std::string &content, const std::string &json)
 {
     const Qentem::Value<char> value = Qentem::JSON::Parse(json.c_str(), json.length());
     Qentem::StringStream<char> temp = Qentem::Template::Render(content.c_str(), content.length(), &value);
-    return std::string(temp.Storage(), temp.Length());
+    return std::string(temp.First(), temp.Length());
 }
 
 EMSCRIPTEN_BINDINGS(jqen_module)

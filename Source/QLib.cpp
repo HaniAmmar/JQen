c3c23c6383885893efaec2ccd9c0f4f257f6bf96
Hani Ammar <hani.code@outlook.com>
2021-08-25 18:18:12 +0300

    Add JQEN_ENABLE_TESTS

--- Source/QLib.cpp
+++ Source/QLib.cpp
@@ -13,7 +13,30 @@ std::string JQen_Render(const std::string &content, const std::string &json)
     return std::string(temp.First(), temp.Length());
 }
 
+#ifdef JQEN_ENABLE_TESTS
+#define QENTEM_ENABLE_COLORED_OUTPUT 0
+
+#include <sstream>
+static std::wstringstream jqen_ss;
+
+#define QENTEM_OUTPUT_STREAM_TYPE std::wstringstream
+#define QENTEM_OUTPUT_STREAM jqen_ss
+#include "Test.hpp"
+
+std::wstring JQen_RunTests()
+{
+    Qentem::Test::TestHelper::Init();
+    Qentem::Test::PrintInfo();
+    Qentem::Test::RunTests();
+
+    return jqen_ss.str();
+}
+#endif
+
 EMSCRIPTEN_BINDINGS(jqen_module)
 {
     function("JQen_Render", &JQen_Render);
+#ifdef JQEN_ENABLE_TESTS
+    function("JQen_RunTests", &JQen_RunTests);
+#endif
 }

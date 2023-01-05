125625bf8bca52ba261b4d662d9a6eeb8977bb12
HaniAmmar <hani.code@outlook.com>
2023-01-05 23:19:44 +0300

    Update Qlib

--- Source/QLib.cpp
+++ Source/QLib.cpp
@@ -9,8 +9,8 @@ using namespace emscripten;
 std::string JQen_Render(const std::string &content, const std::string &json)
 {
     const Qentem::Value<char> value = Qentem::JSON::Parse(json.c_str(), json.length());
-    const Qentem::StringStream<char> temp = Qentem::Template::Render(content.c_str(), content.length(), &value);
-    return std::string(temp.First(), temp.Length());
+    const Qentem::StringStream<char> stream = Qentem::Template::Render(content.c_str(), content.length(), value);
+    return std::string(stream.First(), stream.Length());
 }
 
 #ifdef JQEN_ENABLE_TESTS
